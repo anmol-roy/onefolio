c4bcf4e35bce939233c90edb1d0240f3b0e7d4aa
Anmol Kumar <royanmol112@gmail.com>
2026-09-13 16:32:49 +0530

    feat: implement DashboardHeader component and update MainLayout

--- components/layout/main-layout.tsx
+++ components/layout/main-layout.tsx
@@ -1,15 +1,15 @@
-import ConcentrationMap from "../dashboard/concentration-map";
-import DashBoardHeader from "../dashboard/dashboard-header";
-import SummaryCards from "../dashboard/SummaryCards";
-import Table from "../table";
+import DashboardHeader from "@/components/DashboardHeader";
+import SummaryCards from "@/components/dashboard/SummaryCards";
+import { Portfolio } from "@/lib/portfolio";
+import { calculatePortfolioSummary } from "@/lib/summaryCalculation";
 
 export default function MainLayout() {
-    return(
-        <main className="p-12" >
-            <DashBoardHeader />
-            <SummaryCards summary={{} as any} />
-            <Table />
-            <ConcentrationMap />
-        </main>
-    ) 
+  const summary = calculatePortfolioSummary(Portfolio);
+
+  return (
+    <main className="flex-1 bg-[#e9e3da] p-6 md:p-8 lg:p-10">
+      <DashboardHeader />
+      <SummaryCards summary={summary} />
+    </main>
+  );
 }
